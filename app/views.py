1d7699d207c1e16a7d06cc6491d8144abbe3fd70
Tanmay <tanmaykacker40@gmail.com>
2016-09-19 16:14:51 +0530

    Adds basic templating
    Adds the capability to display dynamic content.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
+def main():
+	return 'Yup, This works'
+
 @app.route('/index')
 def index():
-	return 'Hello World'
+	user = {'nickname' : 'BlueTrainColtrane'}
+	return render_template('index.html', title = 'Home', user = user)
